5f996f38d5400052b4ff2fd57b81d6c11efd1b71
Voldan09 <vol-dan@yandex.ru>
2019-04-14 12:54:31 +0500

    Create BathRoomEL.ino

--- BathRoomEL/BathRoomEL.ino
+++ BathRoomEL/BathRoomEL.ino
@@ -2,12 +2,19 @@
 
 int ledPin = 5;
 int SensorPin = 2;
+int SensorPin2 = 3;
+int toiletLed = 9;
+int toiletSensor = 8;
 int val = 0;
 int bath = 0;   // статус света в ванной
 int bathVal = 0;// мгновенное состояние
 int bathTimer = 5; //секунды
 int curentBathTimer = 0;
-int prog[] = {0,90,160,255}; // программы освещения
+int toilet = LOW;
+int toiletVal = HIGH;
+int toiletTimer = 10;
+int curentToiletT = 0;
+int prog[] = {0,90,160,255}; // программы освещения ванной
 int curentProg = 3;
 
 void setup() {
@@ -25,6 +32,7 @@ void timer_handle_interrupts(int timer) {
   if (digitalRead(SensorPin)){
     bath = prog[curentProg];
     curentBathTimer = bathTimer * 50;
+    //digitalWrite(13, !digitalRead(13));
   }
    else {
     if (curentBathTimer > 0) {--curentBathTimer;} else {bath = 0;}
@@ -32,5 +40,4 @@ void timer_handle_interrupts(int timer) {
    
     if (bathVal < bath) bathVal += 2;
     if (bathVal > bath) --bathVal;
-    //digitalWrite(13, !digitalRead(13));
 }
